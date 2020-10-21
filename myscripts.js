14a9c81a3c1f9114098c88d0b0ef9b55a2d63e8c
alexanderalban <albanalexander@gmail.com>
2020-10-21 14:04:26 -0500

    initial commit, code runs

---
+++ myscripts.js
@@ -0,0 +1,25 @@
+const secondsInAMinute = 60;
+const minutesInAnHour = 60;
+
+const secondsInAnHour = secondsInAMinute * minutesInAnHour;
+
+console.log(secondsInAnHour);
+
+
+const hoursInADay = 24;
+const secondsInADay = secondsInAnHour * hoursInADay;
+
+console.log(secondsInADay);
+
+
+const daysInAYear = 365;
+const secondsInAYear = secondsInADay * daysInAYear;
+
+console.log(secondsInAYear);
+
+let age = 31;
+let myAgeInSeconds = secondsInAYear * age;
+
+console.log(myAgeInSeconds);
+
+// type this in console!
